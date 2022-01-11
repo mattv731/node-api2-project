b44a40a9e16a6bb75f6e1634185199a2227cd40e
abc <abc@example.com>
2022-01-11 16:10:30 -0700

    first commit

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,16 @@
 // implement your posts router here
+const express = require('express')
+const Posts = require('./posts-model.js')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    Posts.find(req.query)
+    .then(posts => {
+        res.status(200).json(posts)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({ message})
+    })
+})
+
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // require your server and launch it here
+const server = ('./api/server.js')
+
+const port = 9000
+
+server.listen(port, () => {
+    console.log(`server running on port ${port}`)
+})
